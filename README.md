c3dbf8c70c190ec2812573c857c26913b8534b1c
Flow <hflowhigh@gmail.com>
2023-10-21 21:12:50 +0330

    Update

--- README.md
+++ README.md
@@ -1,28 +1,8 @@
-# SimpleHitbox
-Create simple box shaped hitboxes for humanoid detection.
+# Priority4
+Priority based state machine for Roblox
 
-# API Document
-### Properties
-> * module.VISUALIZE_HITBOX: boolean (client-only)
-> if true, server will tell the client to visualize the hitbox
-
-### Functions
-> * module.new: (part: BasePart, offset: CFrame, size: Vector3, interval: number, overlapParams: OverlapParams) -> ({Connection: RBXScriptConnection, HumanoidDetected: RBXScriptSignal})
+# Info
+https://devforum.roblox.com/t/priority4-priority-based-state-machine/2614734
 
 # Roblox Marketplace
-https://create.roblox.com/marketplace/asset/15117466504/SimpleHitbox
-
-# Example Script
-```lua
-local offset = CFrame.new(0, 0, -3)
-local size = Vector3.new(3, 6, 3)
-local hitbox = module.new(character.HumanoidRootPart, offset, size, 0.15)
-
-hitbox.HumanoidDetected:Connect(function(humanoid)
-	humanoid:TakeDamage(10)
-end)
-
-task.wait(1)
-
-hitbox.Connection:Disconnect()
-```
+https://www.roblox.com/library/14858563547/Priority4
--- src/init.lua
+++ src/init.lua
@@ -1,165 +1,230 @@
---// Services
 local RunService = game:GetService("RunService")
+local BridgeNet2 = require(script:WaitForChild("Packages"):WaitForChild("BridgeNet2"))
 
---[[
-
-	-- Example Code --
-
-	local offset = CFrame.new(0, 0, -3)
-	local size = Vector3.new(3, 6, 3)
-	local hitbox = module.new(humanoidRootPart, offset, size, 0.15)
-
-	hitbox.HumanoidDetected:Connect(function(humanoid)
-		humanoid:TakeDamage(10)
-	end)
-
-	task.wait(1)
-
-	hitbox.Connection:Disconnect()
-	
-	-- Example Code --
-	
-]]
-
---// Configs
-local HITBOX_VISUALIZER = Instance.new("Part")
-HITBOX_VISUALIZER.Anchored = true
-HITBOX_VISUALIZER.CanCollide = false
-HITBOX_VISUALIZER.CanQuery = false
-HITBOX_VISUALIZER.CanTouch = false
-HITBOX_VISUALIZER.Transparency = 0.75
-HITBOX_VISUALIZER.Material = Enum.Material.Neon
-HITBOX_VISUALIZER.Color = Color3.new(1, 0, 0)
-
---// Modules
-local Signal = require(script:WaitForChild("Packages"):WaitForChild("signal"))
-local BridgeNet2 = require(script:WaitForChild("Packages"):WaitForChild("bridgenet2"))
-local PartCache = require(script:WaitForChild("partcache"))
-local Squash = require(script:WaitForChild("Packages"):WaitForChild("squash"))
-
---// Variables
-local partCache = PartCache.new(HITBOX_VISUALIZER, 150)
-local bridge = BridgeNet2.ReferenceBridge("Hitbox")
-local bridgeActions = {
-	Visualize = 0,
+-- Proxcies
+local machineProxy = {
+	-- example:
+	-- state1 = {Type = "State", Value = State}
+	-- state2 = {Type = "State", Value = State}
 }
 
-local module = {}
-module.VISUALIZE_HITBOX = true
+local stateProxy = {
+	Priority = {Readonly = false, Value = 0},
+	Enabled = {Readonly = false, Value = false},
+	Active = {Readonly = true, Value = false},
+	Changed = {Readonly = true, Value = "Instance"},
+}
 
-local replicating = {} -- players that want hitbox to be visualized
-local proxy: typeof(module) = newproxy(true)
-local meta = getmetatable(proxy)
-meta.__index = module
-meta.__newindex = function(_, i, v)
-	if module[i] == v then return end
+-- Variables
+local bridge = BridgeNet2.ReferenceBridge("Priority4")
+local module = {}
+module.memory = {}
+module.replicating = {}
+
+-- Private Functions
+local function deepCopy(original)
+	local copy = {}
+	for k, v in pairs(original) do
+		if type(v) == "table" then
+			v = deepCopy(v)
+		end
+		copy[k] = v
+	end
+	return copy
+end
+
+-- Module Functions
+function module.CreateMachine(object: Instance): StateMachine
+	local machineProxy = deepCopy(machineProxy)
+	local meta = {}
 	
-	if RunService:IsClient() and i == "VISUALIZE_HITBOX" then
-		bridge:Fire({bridgeActions.Visualize, v})
+	assert(object, "First argument for CreateMachine is nil.")
+	
+	if RunService:IsClient() then
+		bridge:Fire({"get", object})
 	end
 	
-	module[i] = v
-end
+	machineProxy["CreateState.Hidden"] = function(i)
+		local stateProxy = deepCopy(stateProxy)
+		local meta = {}
 
---// Functions
---[[
+		stateProxy["Changed.Hidden"] = Instance.new("BindableEvent")
 
-	@ module.new()
+		meta.__index = function(t, i)
+			local exists = stateProxy[i]
 
-	-- Arguments:
-		part: BasePart,
-		offset: CFrame,
-		size: Vector3,
-		interval: number,
-		overlapParams: OverlapParams,
+			if exists then
+				return exists.Value
+			end
+		end
+
+		meta.__newindex = function(t, i, v)
+			if i == "Updated.Hidden" and v == true then
+				-- private property that fires Changed signal
+				stateProxy["Changed.Hidden"]:Fire(stateProxy.Enabled.Value, stateProxy.Active.Value)
+				return
+			end
+
+			if stateProxy[i] ~= nil and typeof(stateProxy[i].Value) == typeof(v) then
+				stateProxy[i].Value = v
+				machineProxy["Update.Hidden"]()
+			end
+		end
+
+		meta.__tostring = function(t)
+			local str = ""
+			str = str .. tostring((stateProxy.Enabled.Value == true and "🟢") or "⚫")
+			str = str .. tostring((stateProxy.Active.Value == true and "🟢") or "⚫")
+			return str
+		end
+
+		local state = setmetatable({}, meta)
+		rawset(state, "Proxy.Hidden", stateProxy)
+		stateProxy.Changed.Value = stateProxy["Changed.Hidden"].Event
+
+		machineProxy[i] = {Type = "State", Value = state}
+		
+		return machineProxy[i]
+	end
 	
-	-- Returns:
-		{
-			Connection: RBXScriptConnection,
-			HumanoidDetected: RBXScriptSignal,
-		}
+	machineProxy["Update.Hidden"] = function()
+		local enabledClasses = {}
 
-]]
-function module.new(part: BasePart, offset: CFrame, size: Vector3, interval: number, overlapParams: OverlapParams)
-	local connection: RBXScriptConnection -- connection of hitbox loop
-	local last_hitbox_made = 0 -- last time we made a hitbox
-	
-	local humanoids = {} -- list of detected humanoids
-	local detected = Signal.new() -- fire this event when you detect a new humanoid
-	
-	connection = RunService.Heartbeat:Connect(function()
-		-- Make a hitbox every "intervals" seconds
-		if time() - last_hitbox_made > interval then
-			local cframe = part.CFrame*offset
-			local parts = workspace:GetPartBoundsInBox(cframe, size, overlapParams)
-			
-			-- Tell clients to visualize the hitbox using a part
-			bridge:Fire(BridgeNet2.Players(replicating), {
-				bridgeActions.Visualize,
-				Squash.CFrame.ser(cframe),
-				Squash.Vector3.ser(size),
-			})
-			
-			for _, v in parts do
-				-- Check all parts to find their humanoids
-				local character = v.Parent
-				local humanoid = character:FindFirstChildWhichIsA("Humanoid")
-				
-				if humanoid and not humanoids[humanoid] then
-					-- Fire detected event for humanoid
-					detected:Fire(humanoid)
-					humanoids[humanoid] = true
+		for i, state in machineProxy do
+			if typeof(state) == "table" and state.Type == "State" then
+				if state.Value.Enabled then
+					table.insert(enabledClasses, {i, state.Value.Priority})
 				end
 			end
-			
-			last_hitbox_made = time()
 		end
-	end)
-	
-	return {
-		-- return the connection so we can :Disconnect() it anytime we want
-		Connection = connection,
-		-- return the detected event so we can use :Connect on it when we detect a humanoid
-		HumanoidDetected = detected,
-	}
-end
 
-if RunService:IsServer() then
-	-- Server only code
-	bridge:Connect(function(player, content)
-		local action = content[1]
+		table.sort(enabledClasses, function(a,b)
+			return a[2] > b[2]
+		end)
 		
-		if action == bridgeActions.Visualize  then
-			local boolean = table.unpack(content, 2)
-			local exists = table.find(replicating, player)
+		for i, state in machineProxy do
+			if typeof(state) == "table" and state.Type == "State" then
+				if #enabledClasses > 0 and enabledClasses[1][1] == i then
+					-- highest priority
+					rawget(state.Value, "Proxy.Hidden").Active.Value = true
+				else
+					-- not highest priority
+					rawget(state.Value, "Proxy.Hidden").Active.Value = false
+				end
 
-			if boolean ~= true and exists then
-				table.remove(replicating, exists)
-			elseif boolean == true and not exists then
-				table.insert(replicating, player)
+				state.Value["Updated.Hidden"] = true
 			end
 		end
-	end)
-else
-	-- Client only code
-	bridge:Fire({bridgeActions.Visualize ,module.VISUALIZE_HITBOX})
+	end
+	
+	meta.__index = function(t, i)
+		if i == "meta" then
+			return machineProxy
+		end
+		
+		local exists = machineProxy[i] -- check if indexed item exists
+		
+		if exists then
+			if exists.Type == "State" then
+				-- Return State
+
+				return exists.Value
+			end
+		else
+			return machineProxy["CreateState.Hidden"](i).Value
+		end
+	end
+	
+	meta.__newindex = function(t, i, v)
+		local exists = machineProxy[i] -- check if indexed item exists
+		
+		if not exists then
+			-- Create State
+			exists = machineProxy["CreateState.Hidden"](i)
+		end
+		
+		if exists and exists.Type == "State" then
+			-- Change State Proprty
+			
+			if typeof(v) == "boolean" and exists.Value.Enabled ~= v then
+				exists.Value.Enabled = v
+			elseif typeof(v) == "number" and exists.Value.Priority ~= v then
+				exists.Value.Priority = v
+			end
+			
+			if RunService:IsServer() then
+				for player: Player, objects: {Instance} in module.replicating do
+					if table.find(objects, object) then
+						bridge:Fire(player, {"set", object, i, v})
+					end
+				end
+			end
+		end
+	end
+	
+	meta.__tostring = function(t)
+		local str = ""
+		
+		for i, v in machineProxy do
+			if typeof(v) == "table" and v.Type == "State" then
+				str = str .. i .. ":" .. tostring(v.Value) .. " "
+			end
+		end
+		
+		return str
+	end
+	
+	local machine = setmetatable({}, meta)
+	
+	module.memory[object] = machine
+	
+	return machine
+end
+
+function module.GetMachine(object: Instance)
+	return module.memory[object] or module.CreateMachine(object)
+end
+
+if RunService:IsClient() then
 	bridge:Connect(function(content)
 		local action = content[1]
 		
-		if action == bridgeActions.Visualize then
-			local serializedCFrame, serializedSize = table.unpack(content, 2)
-			local cframe, size = Squash.CFrame.des(serializedCFrame), Squash.Vector3.des(serializedSize)
-
-			local part = partCache:GetPart()
-			part.CFrame = cframe
-			part.Size = size
-			part.Parent = workspace
-
-			task.delay(0.75, function()
-				partCache:ReturnPart(part)
-			end)
+		if action == "set" then
+			local object, i, v = table.unpack(content, 2)
+			local machine = module.GetMachine(object)
+			
+			if machine then
+				machine[i] = v
+			end
+		end
+	end)
+elseif RunService:IsServer() then
+	bridge:Connect(function(player, content)
+		local action = content[1]
+		
+		if action == "get" then
+			local object = table.unpack(content, 2)
+			local machine = module.GetMachine(object)
+			
+			if module.replicating[player] == nil then
+				module.replicating[player] = {}
+			end
+			
+			table.insert(module.replicating[player], object)
+			
+			for c, b in machine.meta do
+				if typeof(b) == "table" and b.Type == "State" then
+					bridge:Fire(player, {"set", object, c, b.Value.Enabled})
+					bridge:Fire(player, {"set", object, c, b.Value.Priority})
+				end
+			end
 		end
 	end)
 end
 
-return proxy
+-- Types
+export type StateMachine = typeof(machineProxy)
+export type State = typeof(stateProxy)
+
+-- Return Module
+return module
